9a41886d82cc03f5c4d493afaec86a5cce7a0032
Camilo Gonz <camilogit123@gmail.com>
2020-07-01 23:24:39 -0600

    fixing something lastest push

--- src/main/java/com/inw/proy/utils/CheckPublicEndPoints.java
+++ src/main/java/com/inw/proy/utils/CheckPublicEndPoints.java
@@ -1,9 +1,7 @@
 package com.inw.proy.utils;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.inw.proy.configurations.PathEnviroment;
 
 @Component
 public class CheckPublicEndPoints {
